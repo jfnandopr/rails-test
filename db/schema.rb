5ea18d3eda1c7362604a846e298adf295df7b8c2
Jemerson Fernando Maia <jfnandopr@hotmail.com>
2023-01-15 21:41:42 -0300

    Update db/schema.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_15_133128) do
   create_table 'pets', force: :cascade do |t|
-    t.string "name"
+    t.string 'name'
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
